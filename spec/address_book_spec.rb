04faedbb1f7168e8ddcb27297b81dfcdf798880f
Dan Kang <dtkang@gmail.com>
2015-11-01 04:25:09 +0000

    Assignment 21 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -43,40 +43,65 @@ RSpec.describe AddressBook do
     end
     
     describe "#import_from_csv" do
-        it "imports the correct number of entries" do
+        it "imports the correct number of entries from entries.csv" do
             book.import_from_csv("entries.csv")
             book_size = book.entries.size
             expect(book_size).to eql 5
         end
         
-        it "imports the 1st entry" do
+        it "imports the 1st entry from entries.csv" do
             book.import_from_csv("entries.csv")
             entry_one = book.entries[0]
             check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
         end
         
-        it "imports the 2nd entry" do
+        it "imports the 2nd entry from entries.csv" do
             book.import_from_csv("entries.csv")
             entry_two = book.entries[1]
             check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
         end
  
-        it "imports the 3rd entry" do
+        it "imports the 3rd entry from entries.csv" do
             book.import_from_csv("entries.csv")
             entry_three = book.entries[2]
             check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
         end
  
-        it "imports the 4th entry" do
+        it "imports the 4th entry from entries.csv" do
             book.import_from_csv("entries.csv")
             entry_four = book.entries[3]
             check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
         end
      
-        it "imports the 5th entry" do
+        it "imports the 5th entry from entries.csv" do
             book.import_from_csv("entries.csv")
             entry_five = book.entries[4]
             check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
-        end        
+        end
+        
+        it "imports the correct number of entries from entries_2.csv" do
+            book.import_from_csv("entries_2.csv")
+            book_size = book.entries.size
+            expect(book_size).to eql 3
+        end
+        
+        it "imports the 1st entry from entries_2.csv" do
+            book.import_from_csv("entries_2.csv")
+            entry_one = book.entries[0]
+            check_entry(entry_one, "BillyBob", "555-555-4853", "billybob@blocmail.com")
+        end
+        
+        it "imports the 2nd entry from entries_2.csv" do
+            book.import_from_csv("entries_2.csv")
+            entry_two = book.entries[1]
+            check_entry(entry_two, "Dean", "555-555-3669", "dean@blocmail.com")
+        end
+ 
+        it "imports the 3rd entry from entries_2.csv" do
+            book.import_from_csv("entries_2.csv")
+            entry_three = book.entries[2]
+            check_entry(entry_three, "Jimbo", "555-555-5414", "jimbo@blocmail.com")
+        end
+        
     end
 end
